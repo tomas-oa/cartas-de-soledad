8b87481991ec5690397b74ad623abb742f38c476
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-26 01:06:47 -0300

    feat: :bug: toast displays twice when theres no answer

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -13,7 +13,10 @@ export default function TextArea () {
 
   useEffect(() => {
     if (textAreaRef.current === null) return
-    if (textAreaRef.current.value === '') return
+    if (textAreaRef.current.value === '') {
+      toast.error('¡No puedes enviar una respuesta vacía!')
+      return
+    }
 
     if (!pending && alreadySent.current) {
       alreadySent.current = false
@@ -30,12 +33,12 @@ export default function TextArea () {
       <textarea
         ref={textAreaRef}
         name='answer'
-        className="bg-slate-100 text-[#333] w-full h-72 p-4 resize-none rounded-md border border-slate-200 shadow-sm focus:outline focus:outline-offset-0 focus:outline-2 focus:outline-slate-300"
+        className="bg-slate-100 text-[#333] w-full h-52 sm:h-72 p-2 px-3 sm:p-4 resize-none rounded-md border border-slate-200 shadow-sm focus:outline focus:outline-offset-0 focus:outline-2 focus:outline-slate-300"
         placeholder="Escribe aquí..."
       >
         {pending ? <Spinner /> : ''}
       </textarea>
-      <button type='submit' disabled={pending} className="w-9 grid place-content-center absolute bottom-4 right-5 aspect-square rounded-full border bg-[#375eb8] hover:scale-110 hover:transition-all disabled:opacity-25 focus:scale-110 focus:transition-all">
+      <button type='submit' disabled={pending} className="w-9 grid place-content-center absolute bottom-4 right-2 sm:right-5 aspect-square rounded-full border bg-[#375eb8] hover:scale-110 hover:transition-all disabled:opacity-25 focus:scale-110 focus:transition-all">
         <Send />
       </button>
     </>
