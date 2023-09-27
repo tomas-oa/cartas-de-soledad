ba11279c9ac341c1196e5e1553fc5594aa9ae65f
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-09-27 14:32:59 -0300

    fix: :bug: background color
    applied bg-color to all the screen

--- src/app/677fe21b65f4a6cf1d105853d51f4f0a562698aa6374cb4d279d46a99dd3e6a3/layout.tsx
+++ src/app/677fe21b65f4a6cf1d105853d51f4f0a562698aa6374cb4d279d46a99dd3e6a3/layout.tsx
@@ -13,9 +13,11 @@ export default function DashboardLayout ({
   children: React.ReactNode
 }) {
   return (
-    <section className="min-h-screen w-5/6 mx-auto grid place-content-center gap-12 mt-12 bg-[#fffef5]">
-      <h1 className={`${calistoga.className} text-7xl text-center`}>Cartas recibidas</h1>
-      {children}
+    <section className='bg-[#fffef5] min-w-full'>
+      <div className="min-h-screen w-5/6 mx-auto grid place-content-center gap-12 mt-12 ">
+        <h1 className={`${calistoga.className} text-7xl text-center`}>Cartas recibidas</h1>
+        {children}
+      </div>
     </section>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout ({
 }) {
   return (
     <html lang="es">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} bg-[#fffef5]`}>
         {children}
         <Toaster />
       </body>
