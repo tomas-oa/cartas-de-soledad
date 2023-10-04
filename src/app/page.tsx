1d05e53d9e90476252dcc0e9ab5a5f3272441a98
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-04 20:45:16 -0300

    feat(layout): :lipstick: modified padding and gap
    to fit better on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import StampDesktop from '@/components/StampDesktop'
 export default function App () {
   return (
     <div className="grid-cols-1 w-5/6 mx-auto min-h-[100svh] grid place-content-center gap-2 md:grid-cols-[70%,1fr]">
-      <section className='grid gap-8 md:gap-8 py-2 md:pb-0'>
+      <section className='grid gap-6 md:gap-8 py-2 pt-10 md:pb-0'>
         <Header />
         <Form />
       </section>
