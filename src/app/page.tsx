2f2bb01f5aa03210c24c50fc43ae5d120d7b2d45
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-04 19:58:59 -0300

    feat(layout): :lipstick: dvh to svh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import StampDesktop from '@/components/StampDesktop'
 
 export default function App () {
   return (
-    <div className="grid-cols-1 w-5/6 mx-auto min-h-[100dvh] grid place-content-center gap-2 md:grid-cols-[70%,1fr]">
+    <div className="grid-cols-1 w-5/6 mx-auto min-h-[100svh] grid place-content-center gap-2 md:grid-cols-[70%,1fr]">
       <section className='grid gap-8 md:gap-8 pt-14 md:pt-0'>
         <Header />
         <Form />
