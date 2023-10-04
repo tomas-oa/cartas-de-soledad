eedaa42a4b83996c5224c7a741f50f74aa584bc2
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-04 18:17:17 -0300

    refactor(main page): :art: header + form components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,30 +7,18 @@ export default async function Form () {
   const submitAnswer = async (form: FormData) => {
     'use server'
     const answer = form.get('answer')
-    const doesConsent = form.get('does_consent') ?? false
 
     if (answer === '') return
 
     const supabase = createServerActionClient({ cookies })
     await supabase
       .from('answers')
-      .insert({ response: answer, does_consent: doesConsent })
+      .insert({ response: answer })
   }
 
   return (
     <form action={submitAnswer}>
       <TextArea />
-      <div className="flex gap-4 mt-4 text-left items-center">
-        <input
-          className="bg-[#375eb8] w-4 h-4 rounded-md"
-          id="does_consent"
-          name="does_consent"
-          type="checkbox"
-        />
-        <label className="text-sm font-bold" htmlFor="does_consent">
-          Acepto que un extracto de mi carta pueda publicarse en redes sociales de manera anónima.
-        </label>
-      </div>
     </form>
   )
 }
