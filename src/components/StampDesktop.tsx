7a7cd64b46d54995cc8ec9aab82081422a281da8
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-04 18:13:50 -0300

    feat(images): :sparkles: added stamps and info icon

---
+++ src/components/StampDesktop.tsx
@@ -0,0 +1,7 @@
+export default function StampDesktop () {
+  return (
+    <section className='relative hidden md:block'>
+      <img className='absolute -rotate-[8deg] aspect-auto top-0 md:-right-4 lg:right-10' src="/estampilla_desktop.webp" alt="estampilla desktop" width={240} />
+    </section>
+  )
+}
---
+++ src/components/StampMobile.tsx
@@ -0,0 +1,5 @@
+export default function StampMobile () {
+  return (
+    <img className='absolute -z-10 top-0 right-0 md:hidden' width={120} src="/estampilla_mobile.webp" alt="estampilla mobile" />
+  )
+}
