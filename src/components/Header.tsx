c65e98b0ca86b5dbe7bb012fd1220f164b000fa7
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-04 22:51:48 -0300

    feat(main page): :lipstick: adjusted header font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,12 +14,10 @@ export default function Header () {
         <h1 className={`${calistoga.className} text-5xl md:text-7xl lg:text-8xl mb-2`}>
           Cuéntame<br />de tu soledad
         </h1>
-        <p className="[text-wrap:balance] leading-snug mt-[2px] text-sm md:mb-0 md:[font-size:1.125rem]">
+        <p className="[text-wrap:balance] leading-snug mt-[2px] text-[0.93rem] md:mb-0 md:[font-size:1.125rem]">
           Todos hemos experimentado la soledad en algún momento. Pero, ¿qué hay
           detrás de esa sensación? ¿Cómo definirías y describirías la soledad en
           tu propia experiencia? ¿En qué situaciones la experimentas?
-        </p>
-        <p className='[text-wrap:balance] leading-snug mt-[2px] text-sm md:mb-0 md:[font-size:1.125rem]'>
           Comparte tus pensamientos y sentimientos de manera anónima en una carta.
         </p>
       </header>
