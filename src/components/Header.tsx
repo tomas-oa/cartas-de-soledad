831e027cc62ce12b14e117bc3d0c4210e3ded9c7
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-09 14:27:17 -0300

    feat(main page): :lipstick: updated paragraph

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,10 +24,8 @@ export default function Header () {
           Cuéntame<br />de tu soledad
         </h1>
         <p className="[text-wrap:balance] leading-snug mt-[2px] text-[0.93rem] md:mb-0 md:[font-size:1.125rem]">
-          Todos hemos experimentado la soledad en algún momento. Pero, ¿qué hay
-          detrás de esa sensación? ¿Cómo definirías y describirías la soledad en
-          tu propia experiencia? ¿En qué situaciones la experimentas?
-          Comparte tus pensamientos y sentimientos de manera anónima en una carta.
+        Todos hemos experimentado la soledad en algún momento. Pero, ¿qué hay detrás de ese sentimiento? ¿En qué situaciones la has experimentado?
+        ¿Cómo definirías y describirías la soledad en tu propia experiencia? Comparte tus sentimientos y vivencias de manera anónima en una carta.
         </p>
       </header>
     </>
